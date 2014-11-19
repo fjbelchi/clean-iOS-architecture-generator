e389fed93d9029a2108d1897a0053ea8aa96aa9b
fjbelchi <fjbelchi@gmail.com>
2014-11-19 23:45:22 +0100

    Action json parser

--- lib/IOSGen/Base/property.rb
+++ lib/IOSGen/Base/property.rb
@@ -4,11 +4,6 @@ module IOSGen
     class Property
       attr_accessor :type, :name
 
-      def initialize(type, name)
-        @type = type
-        @name = name
-      end
-
       def initialize(params = {})
         @type = params[:type] ||= params['type']
         @name = params[:name] ||= params['name']
--- spec/IOSGen/Base/action_spec.rb
+++ spec/IOSGen/Base/action_spec.rb
@@ -33,4 +33,30 @@ describe IOSGen::Base::Action do
     end
 
   end
+
+  describe '#JSON' do
+
+    json = '{
+      "description": "Dismiss the ViewController when the button is tapped",
+      "return_type": "void",
+      "name": "didTapOnCloseButton:",
+      "arguments": [{
+        "type" : "UIButton",
+        "name" : "closeButton"
+        }]
+    }'
+
+    it 'has to parse JSON' do
+      hash = JSON.parse(json)
+      action = described_class.new(hash)
+      expect(action.description).to eq('Dismiss the ViewController when the button is tapped')
+      expect(action.return_type).to eq('void')
+      expect(action.name).to eq('didTapOnCloseButton:')
+      expect(action.arguments.empty?).to be false
+      property = action.arguments[0]
+      expect(property).to be_kind_of IOSGen::Base::Property
+    end
+
+  end
+
 end
