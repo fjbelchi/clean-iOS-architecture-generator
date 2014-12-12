b415e90a7c6c4c19d0763a8346684dceaf1ec728
fjbelchi <fjbelchi@gmail.com>
2014-12-12 20:22:17 +0100

    Issue-1 : Interactor template

--- lib/ios_gen/generator/objc/objc_formatter.rb
+++ lib/ios_gen/generator/objc/objc_formatter.rb
@@ -4,16 +4,25 @@ module IOSGen
       # Objc Formatter
       class Formatter
         # ViewModel and Interactors
-        attr_accessor :view_model, :interactors
-        # File Names
+        attr_accessor :view_model, :interactors, :interactor
+        # File Names ViewModel
         attr_reader :view_model_header_file_name, :view_model_impl_file_name
         attr_reader :view_model_protocol_file_name
+        # File Names Interactors
+        attr_reader :interactor_header_file_name, :interactor_impl_file_name
+        attr_reader :interactor_protocol_file_name
         # ViewModel Protocol
         attr_reader :view_model_protocol_name, :view_model_protocol_delegate
         # ViewModel Porperties
         attr_reader :view_model_properties_header
         # ViewModel Actions
         attr_reader :view_model_actions_header, :view_model_actions_impl
+        # Interactor Protocol
+        attr_reader :interactor_protocol_name
+        # Interactor Porperties
+        attr_reader :interactor_properties_header
+        # Interactor Actions
+        attr_reader :interactor_actions_header, :interactor_actions_impl
 
         def view_model_header_file_name
           "#{@view_model.name}.h"
@@ -61,10 +70,53 @@ module IOSGen
           actions.chop
         end
 
-        def generate_view_model
-          yield(view_model_protocol_file_name, 'templates/objc/ViewModelProtocol.h.erb')
-          yield(view_model_header_file_name, 'templates/objc/ViewModel.h.erb')
-          yield(view_model_impl_file_name, 'templates/objc/ViewModel.m.erb')
+        def interactor_protocol_name
+          "#{@interactor.name}Protocol"
+        end
+
+        def interactor_properties_header
+          properties = ''
+          @interactor.properties.each do |property|
+            properties += "@property (nonatomic, strong) #{property.type}#{property.name};\n"
+          end
+          properties.chop
+        end
+
+        def interactor_actions_header
+          actions = ''
+          action_formatter = ActionFormatter.new
+          @interactor.actions.each do |action|
+            actions += "#{action_formatter.generate_interface(action)}\n"
+          end
+          actions.chop
+        end
+
+        def interactor_actions_impl
+          actions = ''
+          action_formatter = ActionFormatter.new
+          @interactor.actions.each do |action|
+            actions += "#{action_formatter.generate_implementation(action)}\n"
+          end
+          actions.chop
+        end
+
+        def generate_view_model(&block)
+          block.call(view_model_protocol_file_name, 'templates/objc/ViewModelProtocol.h.erb')
+          block.call(view_model_header_file_name, 'templates/objc/ViewModel.h.erb')
+          block.call(view_model_impl_file_name, 'templates/objc/ViewModel.m.erb')
+          @view_model.interactors.each do |interactor|
+            generate_interactor(interactor, &block)
+          end
+        end
+
+        def generate_interactor(interactor, &block)
+          @interactor = interactor
+          @interactor_protocol_file_name = "#{interactor.name}Protocol.h"
+          @interactor_header_file_name = "#{interactor.name}.h"
+          @interactor_impl_file_name = "#{interactor.name}.m"
+          block.call(@interactor_protocol_file_name, 'templates/objc/InteractorProtocol.h.erb')
+          block.call(@interactor_header_file_name, 'templates/objc/Interactor.h.erb')
+          block.call(@interactor_impl_file_name, 'templates/objc/Interactor.m.erb')
         end
       end
     end
--- spec/ios_gen/generator/objc/objc_formatter_spec.rb
+++ spec/ios_gen/generator/objc/objc_formatter_spec.rb
@@ -87,8 +87,8 @@ describe IOSGen::Generator::Objc::Formatter do
     formatter = described_class.new
     formatter.view_model = view_model
 
-    it 'has to yield 3 times' do
-      expect { |b| formatter.generate_view_model(&b) }.to yield_control.exactly(3).times
+    it 'has to yield 6 times' do
+      expect { |b| formatter.generate_view_model(&b) }.to yield_control.exactly(6).times
     end
 
     file_names = []
@@ -108,9 +108,25 @@ describe IOSGen::Generator::Objc::Formatter do
       expect(file_names[1]).to eq(formatter.view_model_header_file_name)
       expect(templates[1]).to eq('templates/objc/ViewModel.h.erb')
     end
-    it 'has to return implementation file name template' do
+
+    it 'has to return implementation file name and template' do
       expect(file_names[2]).to eq(formatter.view_model_impl_file_name)
       expect(templates[2]).to eq('templates/objc/ViewModel.m.erb')
+    end
+
+    it 'has to return interactor protocol file name and template' do
+      expect(file_names[3]).to eq(formatter.interactor_protocol_file_name)
+      expect(templates[3]).to eq('templates/objc/InteractorProtocol.h.erb')
+    end
+
+    it 'has to return interactor header file name and template' do
+      expect(file_names[4]).to eq(formatter.interactor_header_file_name)
+      expect(templates[4]).to eq('templates/objc/Interactor.h.erb')
+    end
+
+    it 'has to return interactor implementation file name and template' do
+      expect(file_names[5]).to eq(formatter.interactor_impl_file_name)
+      expect(templates[5]).to eq('templates/objc/Interactor.m.erb')
     end
 
   end
