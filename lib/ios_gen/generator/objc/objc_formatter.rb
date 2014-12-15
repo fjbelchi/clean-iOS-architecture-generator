3b48754d312c10f4995cac0b70246c22ff10793c
fjbelchi <fjbelchi@gmail.com>
2014-12-15 12:27:38 +0100

    Issue-1 : ViewModel and Interactor Formatter

--- lib/ios_gen/generator/objc/objc_formatter.rb
+++ lib/ios_gen/generator/objc/objc_formatter.rb
@@ -4,119 +4,19 @@ module IOSGen
       # Objc Formatter
       class Formatter
         # ViewModel and Interactors
-        attr_accessor :view_model, :interactors, :interactor
-        # File Names ViewModel
-        attr_reader :view_model_header_file_name, :view_model_impl_file_name
-        attr_reader :view_model_protocol_file_name
-        # File Names Interactors
-        attr_reader :interactor_header_file_name, :interactor_impl_file_name
-        attr_reader :interactor_protocol_file_name
-        # ViewModel Protocol
-        attr_reader :view_model_protocol_name, :view_model_protocol_delegate
-        # ViewModel Porperties
-        attr_reader :view_model_properties_header
-        # ViewModel Actions
-        attr_reader :view_model_actions_header, :view_model_actions_impl
-        # Interactor Protocol
-        attr_reader :interactor_protocol_name
-        # Interactor Porperties
-        attr_reader :interactor_properties_header
-        # Interactor Actions
-        attr_reader :interactor_actions_header, :interactor_actions_impl
+        attr_accessor :view_model, :interactors
+        # Formatters
+        attr_reader :view_model_formatter, :interactor_formatter
 
-        def view_model_header_file_name
-          "#{@view_model.name}.h"
+        def initialize(view_model_formatter = ViewModelFormatter.new,
+                       interactor_formatter = InteractorFormatter.new)
+          @view_model_formatter = view_model_formatter
+          @interfactor_formatter = interactor_formatter
         end
 
-        def view_model_impl_file_name
-          "#{@view_model.name}.m"
-        end
-
-        def view_model_protocol_file_name
-          "#{@view_model.name}Protocol.h"
-        end
-
-        def view_model_protocol_name
-          "#{@view_model.name}Protocol"
-        end
-
-        def view_model_protocol_delegate
-          "#{@view_model.name}ProtocolDelegate"
-        end
-
-        def view_model_properties_header
-          properties = ''
-          @view_model.properties.each do |property|
-            properties += "@property (nonatomic, strong) #{property.type}#{property.name};\n"
-          end
-          properties.chop
-        end
-
-        def view_model_actions_header
-          actions = ''
-          action_formatter = ActionFormatter.new
-          @view_model.actions.each do |action|
-            actions += "#{action_formatter.generate_interface(action)}\n"
-          end
-          actions.chop
-        end
-
-        def view_model_actions_impl
-          actions = ''
-          action_formatter = ActionFormatter.new
-          @view_model.actions.each do |action|
-            actions += "#{action_formatter.generate_implementation(action)}\n"
-          end
-          actions.chop
-        end
-
-        def interactor_protocol_name
-          "#{@interactor.name}Protocol"
-        end
-
-        def interactor_properties_header
-          properties = ''
-          @interactor.properties.each do |property|
-            properties += "@property (nonatomic, strong) #{property.type}#{property.name};\n"
-          end
-          properties.chop
-        end
-
-        def interactor_actions_header
-          actions = ''
-          action_formatter = ActionFormatter.new
-          @interactor.actions.each do |action|
-            actions += "#{action_formatter.generate_interface(action)}\n"
-          end
-          actions.chop
-        end
-
-        def interactor_actions_impl
-          actions = ''
-          action_formatter = ActionFormatter.new
-          @interactor.actions.each do |action|
-            actions += "#{action_formatter.generate_implementation(action)}\n"
-          end
-          actions.chop
-        end
-
-        def generate_view_model(&block)
-          block.call(view_model_protocol_file_name, 'templates/objc/ViewModelProtocol.h.erb')
-          block.call(view_model_header_file_name, 'templates/objc/ViewModel.h.erb')
-          block.call(view_model_impl_file_name, 'templates/objc/ViewModel.m.erb')
-          @view_model.interactors.each do |interactor|
-            generate_interactor(interactor, &block)
-          end
-        end
-
-        def generate_interactor(interactor, &block)
-          @interactor = interactor
-          @interactor_protocol_file_name = "#{interactor.name}Protocol.h"
-          @interactor_header_file_name = "#{interactor.name}.h"
-          @interactor_impl_file_name = "#{interactor.name}.m"
-          block.call(@interactor_protocol_file_name, 'templates/objc/InteractorProtocol.h.erb')
-          block.call(@interactor_header_file_name, 'templates/objc/Interactor.h.erb')
-          block.call(@interactor_impl_file_name, 'templates/objc/Interactor.m.erb')
+        def generate(&block)
+          @view_model_formatter.view_model = view_model
+          @view_model_formatter.generate(&block)
         end
       end
     end
--- spec/ios_gen/generator/objc/objc_formatter_spec.rb
+++ spec/ios_gen/generator/objc/objc_formatter_spec.rb
@@ -27,108 +27,19 @@ describe IOSGen::Generator::Objc::Formatter do
                                                                  Property.new(type: '^()',
                                                                               name: 'completionBlock')
                                                                 ])])]
+  view_model = ViewModel.new(description: description,
+                             name: name,
+                             properties: properties,
+                             actions: actions,
+                             interactors: interactors)
 
-  describe '#Properties ViewModel' do
+  formatter = described_class.new
+  formatter.view_model = view_model
 
-    view_model = ViewModel.new(description: description,
-                               name: name,
-                               properties: properties,
-                               actions: actions,
-                               interactors: interactors)
-
-    formatter = described_class.new
-    formatter.view_model = view_model
-
-    it 'has view_model_properties_header' do
-      expected_string = '@property (nonatomic, strong) NSIndexPath *selectedIndexPath;'
-      expect(formatter.view_model_properties_header).to eq(expected_string)
-    end
-
-    it 'has view_model_actions_header' do
-      expected_string1 = "// Dismiss the ViewController when the button is tapped\n- (void)didTapOnCloseButton;\n"
-      expected_string2 = "// Mark notification as read when the notification is selected\n- (void)didTapAtIndexPath:(NSIndexPath *)indexPath;"
-      expected_string = expected_string1 + expected_string2
-      expect(formatter.view_model_actions_header).to eq(expected_string)
-    end
-
-    it 'has view_model_actions_impl' do
-      expected_string = "- (void)didTapOnCloseButton\n{\n}\n- (void)didTapAtIndexPath:(NSIndexPath *)indexPath\n{\n}"
-      expect(formatter.view_model_actions_impl).to eq(expected_string)
-    end
-
-    it 'has view_model_header_file_name' do
-      expect(formatter.view_model_header_file_name).to eq('FJBNotificationsViewModel.h')
-    end
-
-    it 'has view_model_impl_file_name' do
-      expect(formatter.view_model_impl_file_name).to eq('FJBNotificationsViewModel.m')
-    end
-
-    it 'has view_model_protocol_name' do
-      expect(formatter.view_model_protocol_name).to eq('FJBNotificationsViewModelProtocol')
-    end
-
-    it 'has view_model_protocol_file_name' do
-      expect(formatter.view_model_protocol_file_name).to eq('FJBNotificationsViewModelProtocol.h')
-    end
-
-    it 'has view_model_protocol_delegate' do
-      expect(formatter.view_model_protocol_delegate).to eq('FJBNotificationsViewModelProtocolDelegate')
-    end
-  end
-
-  describe '#generate_view_model' do
-    view_model = ViewModel.new(description: description,
-                               name: name,
-                               properties: properties,
-                               actions: actions,
-                               interactors: interactors)
-
-    formatter = described_class.new
-    formatter.view_model = view_model
-
+  describe '#generate' do
     it 'has to yield 6 times' do
-      expect { |b| formatter.generate_view_model(&b) }.to yield_control.exactly(6).times
+      expect { |b| formatter.generate(&b) }.to yield_control.exactly(6).times
     end
-
-    file_names = []
-    templates = []
-
-    formatter.generate_view_model do |file_name, template|
-      file_names << file_name
-      templates << template
-    end
-
-    it 'has to return protocol file name and template' do
-      expect(file_names[0]).to eq(formatter.view_model_protocol_file_name)
-      expect(templates[0]).to eq('templates/objc/ViewModelProtocol.h.erb')
-    end
-
-    it 'has to return header file name and template' do
-      expect(file_names[1]).to eq(formatter.view_model_header_file_name)
-      expect(templates[1]).to eq('templates/objc/ViewModel.h.erb')
-    end
-
-    it 'has to return implementation file name and template' do
-      expect(file_names[2]).to eq(formatter.view_model_impl_file_name)
-      expect(templates[2]).to eq('templates/objc/ViewModel.m.erb')
-    end
-
-    it 'has to return interactor protocol file name and template' do
-      expect(file_names[3]).to eq(formatter.interactor_protocol_file_name)
-      expect(templates[3]).to eq('templates/objc/InteractorProtocol.h.erb')
-    end
-
-    it 'has to return interactor header file name and template' do
-      expect(file_names[4]).to eq(formatter.interactor_header_file_name)
-      expect(templates[4]).to eq('templates/objc/Interactor.h.erb')
-    end
-
-    it 'has to return interactor implementation file name and template' do
-      expect(file_names[5]).to eq(formatter.interactor_impl_file_name)
-      expect(templates[5]).to eq('templates/objc/Interactor.m.erb')
-    end
-
   end
 
 end
