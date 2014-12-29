4441075c23a7d2770d343eb9d736ba784e9af099
fjbelchi <fjbelchi@gmail.com>
2014-12-29 10:39:29 +0100

    Initial Commit

---
+++ Demo/iOSCleanArchitectureGenerator/iOSCleanArchitectureGenerator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSCleanArchitectureGenerator
+//
+//  Created by Fran_DEV on 29/12/14.
+//  Copyright (c) 2014 fjbelchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
