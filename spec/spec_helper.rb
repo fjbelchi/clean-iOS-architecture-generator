c711f832ff6168b8df69733280a199a83173fae0
fjbelchi <fjbelchi@gmail.com>
2015-01-02 13:18:32 +0100

    Issue-24 : Change order of require to make SimpleCov pick lib source files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,9 @@ require 'simplecov'
 require 'coveralls'
 require 'thor'
 
-Dir['./lib/**/*.rb'].each { |f| require f }
-
 SimpleCov.start do
   add_filter '/spec/'
 end
 Coveralls.wear!
+
+Dir['./lib/**/*.rb'].each { |f| require f }
